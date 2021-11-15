ecad316bb1518e927e8bb5326286db1a8156d911
asaf240796@gmail.com <Asaf24796>
2021-11-15 12:01:36 +0200

    4. Contact Me Section

---
+++ src/components/ContactMeSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ContactMeSection = () => {
+    return (
+        <div className="ContactMe">
+            <h2>Contact <span>Me</span> </h2>
+            <nav>
+                <ul>
+                    <li><a href="www.linkedin.com/in/asaf-elazar"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://github.com/Asaf240796"> <i class="fab fa-github"></i></a></li>
+                    <li><a href="mailto:asaf240796@gmail.com"><i class="fas fa-at"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default ContactMeSection;
